d5462aa64eada351414436ec1e8a748f8e7c6f50
Ujjwal Singh <156454453+ujjwals2606@users.noreply.github.com>
2025-10-25 17:40:49 +0530

    Add function to move all zeroes to the end of array
    Implemented a function to move all zeroes in the array to the end while maintaining the order of non-zero elements.

---
+++ Move All Zeroes to End.java
@@ -0,0 +1,38 @@
+You are given an array arr[] of non-negative integers. You have to move all the zeros in the array to the right end while maintaining the relative order of the non-zero elements. The operation must be performed in place, meaning you should not use extra space for another array.
+
+Examples:
+
+Input: arr[] = [1, 2, 0, 4, 3, 0, 5, 0]
+Output: [1, 2, 4, 3, 5, 0, 0, 0]
+Explanation: There are three 0s that are moved to the end.
+Input: arr[] = [10, 20, 30]
+Output: [10, 20, 30]
+Explanation: No change in array as there are no 0s.
+Input: arr[] = [0, 0]
+Output: [0, 0]
+Explanation: No change in array as there are all 0s.
+Constraints:
+1 ≤ arr.size() ≤ 105
+0 ≤ arr[i] ≤ 105
+
+
+
+  /**
+ * @param {Number[]} arr
+ * @returns {void}
+ */
+
+class Solution {
+    pushZerosToEnd(arr) {
+        // code here
+        let nonZeroPos = 0;
+        
+        for (let i = 0; i < arr.length; i++) {
+            if (arr[i] !== 0) {
+                [arr[nonZeroPos], arr[i]] = [arr[i], arr[nonZeroPos]];
+                nonZeroPos++;
+            }
+        }
+        
+    }
+}
